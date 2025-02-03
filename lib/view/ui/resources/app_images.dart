df4af4bb7f0d4522877eeec381879f95aafe7fb5
Valerii Novykov <v.novykov@urancompany.com>
2025-02-03 18:24:43 +0200

    update app_images

--- lib/view/ui/resources/app_images.dart
+++ lib/view/ui/resources/app_images.dart
@@ -11,5 +11,9 @@
 abstract final class AppImages {
   static const _imgPath = "assets/images/";
 
+  static const homeIcon = "${_imgPath}home_icon.svg";
   static const placeholderImage = "${_imgPath}placeholder_image.png";
+  static const watchListIcon = "${_imgPath}watch_list_icon.svg";
+  static const moreIcon = "${_imgPath}more_icon.svg";
+  static const watchedIcon = "${_imgPath}watched_icon.svg";
 }
