352446bfae64527590842837cda03eba9cb16b5f
Valerii Novykov <v.novykov@urancompany.com>
2025-02-04 13:05:26 +0200

    update settings provider

--- lib/data/sources/impl_settings_provider.dart
+++ lib/data/sources/impl_settings_provider.dart
@@ -1,21 +1,19 @@
+import '../network/env_provider/env_provider.dart';
 import '../repositories/settings_provider.dart';
-import '../repositories/storage_local_data_source.dart';
 
 class ImplSettingsProvider implements SettingsProvider {
   final Function() _localeResolver; // = () => "en";
-  final StorageLocalDataSource _localStorage;
+  final EnvProvider _envProvider;
 
   ImplSettingsProvider(
       {required String Function() localeResolver,
-      required StorageLocalDataSource localStorage})
-      : _localStorage = localStorage,
+      required EnvProvider envProvider})
+      : _envProvider = envProvider,
         _localeResolver = localeResolver;
 
   @override
   Future<String> getAuthToken() async {
-    final token =
-        await _localStorage.get<String>(key: LocalStorageKeys.authToken);
-    return token ?? '';
+    return _envProvider.apiToken;
   }
 
   @override
