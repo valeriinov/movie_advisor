1ac01555b30ac64e1cc9b4ce7e85b2293cd4f6a4
Valerii Novykov <valerii.novykov@gmail.com>
2025-02-07 11:07:56 +0200

    refactor home screen to use HomeContentSwitcher for dynamic content display

---
+++ lib/view/ui/scenes/home/widgets/home_content_switcher.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sliver_tools/sliver_tools.dart';
+
+import '../../../base/view_model/ext/vm_state_provider_creator.dart';
+import '../../../resources/base_theme/durations/base_durations_ext.dart';
+import '../../home_movies/home_movies_view.dart';
+import '../../home_series/home_series_view.dart';
+import '../home_view_model/home_view_model.dart';
+
+class HomeContentSwitcher extends ConsumerWidget {
+  const HomeContentSwitcher({super.key});
+
+  @override
+  Widget build(context, ref) {
+    final durations = context.baseDurations;
+
+    final vsp = ref.vspFromADProvider(homeViewModelPr);
+
+    final contentMode = vsp.selectWatch((s) => s.contentMode);
+
+    return SliverAnimatedSwitcher(
+      duration: durations.animSwitchPrim,
+      child: contentMode.isMovies ? HomeMoviesView() : HomeSeriesView(),
+    );
+  }
+}
--- lib/view/ui/scenes/home_movies/home_movies_view.dart
+++ lib/view/ui/scenes/home_movies/home_movies_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sliver_tools/sliver_tools.dart';
 
 import '../../../di/injector.dart';
 import '../../base/view_model/ext/vm_state_provider_creator.dart';
+import '../../resources/base_theme/dimens/base_dimens_ext.dart';
 import 'home_movies_view_model/home_movies_view_model.dart';
 
 class HomeMoviesView extends ConsumerWidget {
@@ -10,12 +12,19 @@ class HomeMoviesView extends ConsumerWidget {
 
   @override
   Widget build(context, ref) {
+    final dimens = context.baseDimens;
+
     final vsp = ref.vspFromADProvider(homeMoviesViewModelPr);
 
     vsp.handleState(listener: (prev, next) {
       ref.baseStatusHandler.handleStatus(prev, next);
     });
 
-    return SliverToBoxAdapter(child: Center(child: Text('HomeMovies View')));
+    return SliverPadding(
+      padding: dimens.padTopPrimIns,
+      sliver: MultiSliver(
+        children: [],
+      ),
+    );
   }
 }
