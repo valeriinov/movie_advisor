cb762756ab5b717ffefc410d1a713974f79c9725
Valerii Novykov <valerii.novykov@gmail.com>
2025-02-07 10:30:14 +0200

    update home screen view

--- lib/view/ui/scenes/home/widgets/home_screen_view.dart
+++ lib/view/ui/scenes/home/widgets/home_screen_view.dart
@@ -1,38 +1,39 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../../../di/injector.dart';
 import '../../../base/view_model/ext/vm_state_provider_creator.dart';
-import '../../../resources/base_theme/dimens/base_dimens_ext.dart';
+import '../../../resources/base_theme/nav_bars/base_nav_bars_styles_ext.dart';
+import '../../../resources/locale_keys.g.dart';
 import '../../../widgets/app_bar/main_app_bar.dart';
 import '../../home_movies/home_movies_view.dart';
 import '../../home_series/home_series_view.dart';
 import '../home_view_model/home_view_model.dart';
+import 'home_floating_top_bar.dart';
 
 class HomeScreenView extends ConsumerWidget {
   const HomeScreenView({super.key});
 
   @override
   Widget build(context, ref) {
-    final dimens = context.baseDimens;
+    final navBarStyles = context.baseNavBarsStyles;
 
     final vsp = ref.vspFromADProvider(homeViewModelPr);
-
-    vsp.handleState(listener: (prev, next) {
-      ref.baseStatusHandler.handleStatus(prev, next);
-    });
 
     final contentMode = vsp.selectWatch((s) => s.contentMode);
 
     return Scaffold(
-      appBar: MainAppBar(title: Text('Home Screen')), // TODO: Localize title
-      body: Padding(
-          padding: dimens.padHorPrimIns,
-          child: CustomScrollView(
-            slivers: [
-              contentMode.isMovies ? HomeMoviesView() : HomeSeriesView(),
-            ],
-          )),
+      appBar: MainAppBar(
+        centerTitle: false,
+        titleTextStyle: navBarStyles.appBarSecTitleTextStyle,
+        title: Text(LocaleKeys.homeScreenTitle.tr()),
+      ),
+      body: CustomScrollView(
+        slivers: [
+          HomeFloatingTopBar(),
+          contentMode.isMovies ? HomeMoviesView() : HomeSeriesView(),
+        ],
+      ),
     );
   }
 }
