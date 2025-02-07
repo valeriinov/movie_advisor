cdafac59345634ef52ff4a2892b27a9e63aa8ff2
Valerii Novykov <valerii.novykov@gmail.com>
2025-02-07 16:45:37 +0200

    add suggestions display in HomeSeriesView

--- lib/view/ui/scenes/home_series/home_series_view.dart
+++ lib/view/ui/scenes/home_series/home_series_view.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sliver_tools/sliver_tools.dart';
 
 import '../../../di/injector.dart';
 import '../../base/view_model/ext/vm_state_provider_creator.dart';
+import '../../widgets/home_shared/home_content_skeleton.dart';
+import '../../widgets/home_shared/suggestions_container.dart';
 import 'home_series_view_model/home_series_view_model.dart';
 
 class HomeSeriesView extends ConsumerWidget {
@@ -16,6 +19,18 @@ class HomeSeriesView extends ConsumerWidget {
       ref.baseStatusHandler.handleStatus(prev, next);
     });
 
-    return SliverToBoxAdapter(child: Center(child: Text('HomeSeries View')));
+    final suggestionsContent =
+        vsp.selectWatch((s) => s.suggestedSeries.mediaData.items);
+
+    return vsp.isLoading && !vsp.isInitialized
+        ? HomeContentSkeleton()
+        : MultiSliver(
+            children: [
+              SuggestionsContainer(
+                suggestions: suggestionsContent,
+                onTap: (id) {}, // TODO: Go to series details
+              ),
+            ],
+          );
   }
 }
