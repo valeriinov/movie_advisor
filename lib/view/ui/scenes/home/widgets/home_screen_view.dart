05080d94dd7317ddabd002184265155c29d499d7
Valerii Novykov <valerii.novykov@gmail.com>
2025-02-07 11:08:45 +0200

    update home screen view

--- lib/view/ui/scenes/home/widgets/home_screen_view.dart
+++ lib/view/ui/scenes/home/widgets/home_screen_view.dart
@@ -1,29 +1,21 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../../base/view_model/ext/vm_state_provider_creator.dart';
 import '../../../resources/base_theme/dimens/base_dimens_ext.dart';
 import '../../../resources/base_theme/nav_bars/base_nav_bars_styles_ext.dart';
 import '../../../resources/locale_keys.g.dart';
 import '../../../widgets/app_bar/main_app_bar.dart';
-import '../../home_movies/home_movies_view.dart';
-import '../../home_series/home_series_view.dart';
-import '../home_view_model/home_view_model.dart';
+import 'home_content_switcher.dart';
 import 'home_floating_top_bar.dart';
 
-class HomeScreenView extends ConsumerWidget {
+class HomeScreenView extends StatelessWidget {
   const HomeScreenView({super.key});
 
   @override
-  Widget build(context, ref) {
+  Widget build(BuildContext context) {
     final dimens = context.baseDimens;
 
     final navBarStyles = context.baseNavBarsStyles;
-
-    final vsp = ref.vspFromADProvider(homeViewModelPr);
-
-    final contentMode = vsp.selectWatch((s) => s.contentMode);
 
     return Scaffold(
       appBar: MainAppBar(
@@ -34,7 +26,7 @@ class HomeScreenView extends ConsumerWidget {
       body: CustomScrollView(
         slivers: [
           HomeFloatingTopBar(),
-          contentMode.isMovies ? HomeMoviesView() : HomeSeriesView(),
+          HomeContentSwitcher(),
           SliverPadding(padding: dimens.padBotPrimIns)
         ],
       ),
