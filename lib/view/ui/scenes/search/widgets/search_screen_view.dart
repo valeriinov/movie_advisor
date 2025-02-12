6b988c0867ef5a0ca525c2d357568baf5eb4cced
Valerii Novykov <valerii.novykov@gmail.com>
2025-02-12 12:10:43 +0200

    add search content switcher

--- lib/view/ui/scenes/search/widgets/search_screen_view.dart
+++ lib/view/ui/scenes/search/widgets/search_screen_view.dart
@@ -2,19 +2,16 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:sliver_tools/sliver_tools.dart';
 
 import '../../../base/content_mode_view_model/content_mode.dart';
 import '../../../base/view_model/ext/vm_state_provider_creator.dart';
-import '../../../resources/base_theme/durations/base_durations_ext.dart';
 import '../../../resources/locale_keys.g.dart';
 import '../../../widgets/app_bar/floating_search_bar.dart';
 import '../../../widgets/app_bar/main_app_bar.dart';
 import '../../../widgets/scroll_top_fab.dart';
 import '../../../widgets/scroll_top_listener.dart';
 import '../search_view_model/search_view_model.dart';
-import 'search_movies_view.dart';
-import 'search_series_view.dart';
+import 'search_content_switcher.dart';
 
 class SearchScreenView extends HookConsumerWidget {
   final ContentMode initContentMode;
@@ -23,12 +20,9 @@ class SearchScreenView extends HookConsumerWidget {
 
   @override
   Widget build(context, ref) {
-    final durations = context.baseDurations;
-
     final vspContMode = ref.vspFromADFProvider(
       searchContModeViewModelPr(initContentMode),
     );
-
     final vspFilter = ref.vspFromADProvider(searchFilterViewModelPr);
 
     final contMode = vspContMode.selectWatch((s) => s.mode);
@@ -52,15 +46,9 @@ class SearchScreenView extends HookConsumerWidget {
                   onMoreTap: vspContMode.viewModel.toggleMode,
                   textController: searchFieldController,
                 ),
-                SliverAnimatedSwitcher(
-                  duration: durations.animSwitchPrim,
-                  child: contMode.isMovies
-                      ? SearchMoviesView(
-                          scrollController: scrollController,
-                        )
-                      : SearchSeriesView(
-                          scrollController: scrollController,
-                        ),
+                SearchContentSwitcher(
+                  scrollController: scrollController,
+                  contMode: contMode,
                 ),
               ],
             ),
