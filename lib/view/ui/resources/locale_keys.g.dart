974c53b6547f14594f68b57cb2bdb2f93d1d0629
Valerii Novykov <valerii.novykov@gmail.com>
2025-02-13 16:22:13 +0200

    feat: localize movie and series details screen titles

--- lib/view/ui/resources/locale_keys.g.dart
+++ lib/view/ui/resources/locale_keys.g.dart
@@ -51,5 +51,7 @@ abstract class  LocaleKeys {
   static const searchScreenTitle = 'searchScreenTitle';
   static const emptySearchTitle = 'emptySearchTitle';
   static const emptySearchSubtitle = 'emptySearchSubtitle';
+  static const detailsMovieScreenTitle = 'detailsMovieScreenTitle';
+  static const detailsSeriesScreenTitle = 'detailsSeriesScreenTitle';
 
 }
