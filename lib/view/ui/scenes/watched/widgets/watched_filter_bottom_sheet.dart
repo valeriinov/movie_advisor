b09f8e289524aaa57d720274e0793779e608b1cb
Valerii Novykov <valerii.novykov@gmail.com>
2025-02-18 10:55:05 +0200

    feat: add filter bottom sheet for watched screen

--- lib/view/ui/scenes/watched/widgets/watched_filter_bottom_sheet.dart
+++ lib/view/ui/scenes/watched/widgets/watched_filter_bottom_sheet.dart
@@ -3,12 +3,11 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../base/content_mode_view_model/content_mode.dart';
 import '../../../base/view_model/ext/vm_state_provider_creator.dart';
+import '../../../widgets/filter_bottom_sheet.dart';
 import '../watched_view_model/watched_view_model.dart';
-import 'watched_movies_view.dart';
-import 'watched_series_view.dart';
 
-class WatchedScreenView extends ConsumerWidget {
-  const WatchedScreenView({super.key});
+class WatchedFilterBottomSheet extends ConsumerWidget {
+  const WatchedFilterBottomSheet({super.key});
 
   @override
   Widget build(context, ref) {
@@ -18,6 +17,9 @@ class WatchedScreenView extends ConsumerWidget {
 
     final contentMode = vspContMode.selectWatch((s) => s.mode);
 
-    return contentMode.isMovies ? WatchedMoviesView() : WatchedSeriesView();
+    return FilterBottomSheet(
+      contentMode: contentMode,
+      toggleContentMode: vspContMode.viewModel.toggleMode,
+    );
   }
 }
--- lib/view/ui/scenes/watched/widgets/watched_series_view.dart
+++ lib/view/ui/scenes/watched/widgets/watched_series_view.dart
@@ -9,17 +9,17 @@ import '../../../base/content_mode_view_model/content_mode.dart';
 import '../../../base/view_model/ext/vm_state_provider_creator.dart';
 import '../../../navigation/routes/details_route.dart';
 import '../../../resources/locale_keys.g.dart';
+import '../../../widgets/blurred_bottom_sheet.dart';
 import '../../../widgets/scroll_top_fab.dart';
 import '../../../widgets/scroll_top_listener.dart';
 import '../../../widgets/watch_shared/watch_app_bar.dart';
 import '../../../widgets/watch_shared/watch_content_skeleton.dart';
 import '../../../widgets/watch_shared/watch_screen_content.dart';
 import '../watched_view_model/watched_view_model.dart';
+import 'watched_filter_bottom_sheet.dart';
 
 class WatchedSeriesView extends HookConsumerWidget {
-  final VoidCallback toggleContentMode;
-
-  const WatchedSeriesView({super.key, required this.toggleContentMode});
+  const WatchedSeriesView({super.key});
 
   @override
   Widget build(context, ref) {
@@ -50,8 +50,7 @@ class WatchedSeriesView extends HookConsumerWidget {
           return Scaffold(
             appBar: WatchAppBar(
               title: LocaleKeys.watchedSeriesScreenTitle.tr(),
-              // TODO: Add modal bottom sheet
-              onMoreTap: toggleContentMode,
+              onMoreTap: () => _onMoreTap(context),
             ),
             body: isSkeletonVisible
                 ? WatchContentSkeleton()
@@ -97,6 +96,15 @@ class WatchedSeriesView extends HookConsumerWidget {
       currentPage: loadInfo.mediaData.currentPage,
       isLoading: loadInfo.isNextPageLoading,
       isLastPage: loadInfo.mediaData.isLastPage,
+    );
+  }
+
+  void _onMoreTap(BuildContext context) {
+    showBlurredBottomSheet(
+      isDismissible: false,
+      useRootNavigator: true,
+      context: context,
+      child: WatchedFilterBottomSheet(),
     );
   }
 
