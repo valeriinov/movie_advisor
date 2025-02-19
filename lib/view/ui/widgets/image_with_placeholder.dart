f6ebe80f88a048b76f50a4274616a57f57280709
Valerii Novykov <valerii.novykov@gmail.com>
2025-02-19 22:03:00 +0200

    feat: enhance ImageWithLoader and ImageWithPlaceholder widgets with additional customization options

--- lib/view/ui/widgets/image_with_placeholder.dart
+++ lib/view/ui/widgets/image_with_placeholder.dart
@@ -1,5 +1,6 @@
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_utils/flutter_utils.dart';
 
 import '../resources/app_images.dart';
@@ -12,32 +13,37 @@ import '../resources/app_images.dart';
 ///
 /// The [ImageWithPlaceholder] shows a placeholder image while the main image is loading
 /// and displays the same image if loading fails.
-/// Supports customization for image fit and caching.
+/// Supports customization for image size, shape, fit, and caching.
 ///
 /// Example:
 ///
 /// ```dart
 /// ImageWithPlaceholder(
 ///   imagePath: 'https://example.com/image.png',
+///   size: Size(100, 100),
 ///   fit: BoxFit.cover,
+///   placeholder: Image.asset('assets/images/custom_placeholder.png'),
 /// )
 ///
 /// ImageWithPlaceholder(
 ///   imagePath: 'assets/images/local_image.png',
-///   fit: BoxFit.contain,
-///   placeholderFit: BoxFit.cover,
+///   size: Size(100, 100),
+///   fit: BoxFit.cover,
 /// )
 /// ```
 class ImageWithPlaceholder extends StatelessWidget {
+  /// The path or URL of the image to display.
+  ///
+  /// Automatically detects if the path is a network URL or a local asset.
+  final String? imagePath;
+
   /// Custom [ImageProvider] for loading the image.
   ///
   /// If provided, this takes priority over [imagePath], and [imagePath] will be ignored.
   final ImageProvider? imageProvider;
 
-  /// The path or URL of the image to display.
-  ///
-  /// Automatically detects if the path is a network URL or a local asset.
-  final String? imagePath;
+  /// The size of the image.
+  final Size? size;
 
   /// How the image should be fitted within its container.
   final BoxFit? fit;
@@ -45,10 +51,21 @@ class ImageWithPlaceholder extends StatelessWidget {
   /// The fit type for the placeholder image in case of an error.
   final BoxFit? placeholderFit;
 
-  /// Custom [ImageProvider] for the placeholder image.
+  /// The clip behavior of the image.
+  final Clip clipBehavior;
+
+  /// A custom placeholder widget shown while the image is loading.
   ///
   /// If not provided, the default placeholder image is used.
-  final ImageProvider? placeholderProvider;
+  final Widget? placeholder;
+
+  /// A custom error widget shown if the image fails to load.
+  ///
+  /// If not provided, the default placeholder image is used.
+  final Widget? error;
+
+  /// The shape of the image (e.g., circle, rectangle).
+  final BoxShape? shape;
 
   /// The width used for image caching.
   final int? cacheWidth;
@@ -56,49 +73,80 @@ class ImageWithPlaceholder extends StatelessWidget {
   /// The height used for image caching.
   final int? cacheHeight;
 
+  /// Clears the memory cache when the widget is disposed.
+  final bool clearMemoryCacheWhenDispose;
+
   const ImageWithPlaceholder({
     super.key,
+    this.imagePath,
+    this.imageProvider,
+    this.size,
     this.fit = BoxFit.cover,
     this.placeholderFit = BoxFit.cover,
-    this.imageProvider,
-    this.imagePath,
-    this.placeholderProvider,
+    this.clipBehavior = Clip.antiAlias,
+    this.placeholder,
+    this.error,
+    this.shape,
     this.cacheWidth,
     this.cacheHeight,
-  });
+    this.clearMemoryCacheWhenDispose = false,
+  }) : assert(
+         imagePath != null || imageProvider != null,
+         'Either imagePath or imageProvider must be provided',
+       );
 
   @override
   Widget build(BuildContext context) {
-    final image = imageProvider ??
-        imagePath.createImageProviderFromPath(
-            cacheWidth: cacheWidth, cacheHeight: cacheHeight);
-    final placeholder =
-        placeholderProvider ?? const AssetImage(AppImages.placeholderImage);
-
     return ExtendedImage(
-      image: image,
+      image: _createImageProvider(),
       fit: fit,
-      loadStateChanged: (state) => _handleLoadState(state, placeholder),
+      width: size?.width,
+      height: size?.height,
+      shape: shape,
+      clipBehavior: clipBehavior,
+      clearMemoryCacheWhenDispose: clearMemoryCacheWhenDispose,
+      loadStateChanged: (state) => _handleLoadState(state),
     );
   }
 
-  Widget? _handleLoadState(
-    ExtendedImageState state,
-    ImageProvider placeholder,
-  ) {
+  ImageProvider _createImageProvider() {
+    if (imagePath?.isRemoteLink() == true) {
+      return ExtendedNetworkImageProvider(imagePath!, cache: true, scale: 1.0);
+    }
+
+    return imageProvider ??
+        imagePath.createImageProviderFromPath(
+          cacheWidth: cacheWidth,
+          cacheHeight: cacheHeight,
+        );
+  }
+
+  Widget? _handleLoadState(ExtendedImageState state) {
     return switch (state.extendedImageLoadState) {
-      LoadState.loading => Image(
-          image: placeholder,
-          fit: placeholderFit ?? fit,
-        ),
-      LoadState.completed => ExtendedRawImage(
-          image: state.extendedImageInfo?.image,
-          fit: fit,
-        ),
-      LoadState.failed => Image(
-          image: placeholder,
-          fit: placeholderFit ?? fit,
-        ),
+      LoadState.loading => _buildLoader(),
+      LoadState.completed => _buildLoadedImage(state),
+      LoadState.failed => _buildErrorImage(),
     };
+  }
+
+  Widget _buildLoader() {
+    return placeholder ?? _placeholderImage();
+  }
+
+  Widget _buildLoadedImage(ExtendedImageState state) {
+    return ExtendedRawImage(
+      image: state.extendedImageInfo?.image,
+      fit: fit,
+      width: size?.width,
+      height: size?.height,
+    ).animate().fadeIn();
+  }
+
+  Widget _buildErrorImage() {
+    return error ?? _placeholderImage();
+  }
+
+  Widget _placeholderImage() {
+    return Image.asset(AppImages.placeholderImage, fit: placeholderFit ?? fit);
   }
 }
