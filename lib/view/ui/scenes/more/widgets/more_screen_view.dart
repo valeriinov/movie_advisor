94b07fcd16a1aedb1fa566e9367f7f9917db5358
Valerii Novykov <valerii.novykov@gmail.com>
2025-03-04 13:35:36 +0200

    refactor: replace loader with skeleton on Profile screen

--- lib/view/ui/scenes/more/widgets/more_screen_view.dart
+++ lib/view/ui/scenes/more/widgets/more_screen_view.dart
@@ -4,11 +4,11 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../../di/injector.dart';
 import '../../../base/view_model/ext/vm_state_provider_creator.dart';
+import '../../../resources/base_theme/durations/base_durations_ext.dart';
 import '../../../resources/locale_keys.g.dart';
 import '../../../widgets/dialogs/question_dialog.dart';
 import '../more_view_model/more_view_model.dart';
 import 'more_app_bar.dart';
-import 'more_content_skeleton.dart';
 import 'more_screen_content.dart';
 
 class MoreScreenView extends ConsumerWidget {
@@ -16,11 +16,13 @@ class MoreScreenView extends ConsumerWidget {
 
   @override
   Widget build(context, ref) {
+    final durations = context.baseDurations;
+
     final vsp = ref.vspFromADProvider(moreViewModelPr);
 
     final isLoading = vsp.isLoading;
     final isInitialized = vsp.isInitialized;
-    final isSkeletonVisible = isLoading && !isInitialized;
+    final isLoaderVisible = isLoading && !isInitialized;
 
     vsp.handleState(
       listener: (prev, next) {
@@ -37,17 +39,21 @@ class MoreScreenView extends ConsumerWidget {
     return Scaffold(
       appBar: MoreAppBar(),
       resizeToAvoidBottomInset: false,
-      body:
-          isSkeletonVisible
-              ? const MoreContentSkeleton()
-              : MoreScreenContent(
-                user: user,
-                onRefresh:
-                    !isLoading
-                        ? () => vsp.viewModel.loadInitialData(showLoader: false)
-                        : null,
-                onSignOut: () => _showSignOutDialog(context, vsp),
-              ),
+      body: AnimatedSwitcher(
+        duration: durations.animSwitchPrim,
+        child:
+            isLoaderVisible
+                ? Center(child: CircularProgressIndicator())
+                : MoreScreenContent(
+                  user: user,
+                  onRefresh:
+                      !isLoading
+                          ? () =>
+                              vsp.viewModel.loadInitialData(showLoader: false)
+                          : null,
+                  onSignOut: () => _showSignOutDialog(context, vsp),
+                ),
+      ),
     );
   }
 
