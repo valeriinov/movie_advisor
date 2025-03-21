41a951f42b377bd19737860733c93e8ebc6369d8
Valerii Novykov <valerii.novykov@gmail.com>
2025-03-21 11:45:45 +0200

    fix: hide media without posters in home response handling

--- lib/data/network/utils/media_response_handler/media_response_handler.dart
+++ lib/data/network/utils/media_response_handler/media_response_handler.dart
@@ -3,7 +3,13 @@ import '../../../dto/series/series_response_data_dto.dart';
 import '../../network_manager/net_response.dart';
 
 abstract interface class MediaResponseHandler {
-  MoviesResponseDataDto handleMoviesResponse(NetResponse<dynamic> response);
+  MoviesResponseDataDto handleMoviesResponse(
+    NetResponse<dynamic> response, {
+    bool removeWithoutPoster = false,
+  });
 
-  SeriesResponseDataDto handleSeriesResponse(NetResponse<dynamic> response);
+  SeriesResponseDataDto handleSeriesResponse(
+    NetResponse<dynamic> response, {
+    bool removeWithoutPoster = false,
+  });
 }
