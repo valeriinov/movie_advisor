e455d0d68c9b02bbd24a81f2bca6d88b4fc9218a
Valerii Novykov <valerii.novykov@gmail.com>
2025-06-15 14:43:14 +0300

    fix: update size of loaded poster image

--- lib/data/network/constants/movies_api_constants.dart
+++ lib/data/network/constants/movies_api_constants.dart
@@ -1,6 +1,6 @@
 abstract final class MoviesApiConstants {
   static const String backdropSize = 'w780';
-  static const String posterSize = 'w500';
+  static const String posterSize = 'w185';
   static const String creditsSize = 'w185';
   static const String personSize = 'h632';
 }
